b22c953549ccac6b8c01c4514600f425cf84d1cd
subrati_swaroop <subratiswaroop15@gmail.com>
2024-07-13 19:10:15 +0530

    change in index.html

--- myApp.js
+++ myApp.js
@@ -5,15 +5,15 @@ const app = express();
 
 
 
-module.exports = apps;
+module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
-app.use('/_api', api);
+app.use('/_api', x);
 app.get("/",function (request, response){
   response.sendFile(__dirname + '/views/index.html');
 });
-const PORT = process.env.PORT || 3000
+let PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
