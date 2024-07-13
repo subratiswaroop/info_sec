d1327263b3706ac7edf8a60c9ff1277e49a75a48
subrati_swaroop <subratiswaroop15@gmail.com>
2024-07-13 21:55:20 +0530

    add no-cache

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ app.use(helmet.ieNoOpen());
 const timeInSeconds = 90 * 24 * 60 * 60;
 app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
-
+app.use(helmet.noCache());
 // const PORT = process.env.PORT || 3030;
 // app.use(express.urlencoded({extended: true }))
 // app.get('/test', (req, res) => {
