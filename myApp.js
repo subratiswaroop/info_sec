99a0910fc12deac37503d82373043930882055ee
subrati_swaroop <subratiswaroop15@gmail.com>
2024-07-13 17:36:25 +0530

    change app.js

--- myApp.js
+++ myApp.js
@@ -2,15 +2,22 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
+
+
+
+module.exports = apps;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/api', api);
+app.get("/",function (request, response){
+  response.sendFile(__dirname + '/views/index.html');
+});
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
-
-
-
-
 
 
 
