2532ea2f3585d925bb0db4e385c4c0f54e5c009c
subrati_swaroop <subratiswaroop15@gmail.com>
2024-07-13 16:51:34 +0530

    add first application

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
 
 
 
